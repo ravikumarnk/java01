974aab539268b7dfa24b18a02665c20a18a77a48
ravikumar <ravikelakam@gmail.com>
2024-05-20 09:59:41 +0530

    first commit

---
+++ src/test/java/demo1/jenkins01/AppTest.java
@@ -0,0 +1,21 @@
+package demo1.jenkins01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Testing project.. started.. ");
+    }
+    
+}
